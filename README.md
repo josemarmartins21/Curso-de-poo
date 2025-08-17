85c57544e5f726505e1fd5f400ec0c71f680a414
Josimar Martins <josemar21@outlook.pt>
2025-08-17 12:43:56 +0100

    atualizei o curso de POO

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Curso-de-poo
-exercucios do curso de POO de Java e PHP
+exercicios do curso de POO de Java e PHP
---
+++ java/exercicios de logica/exe001.java
@@ -0,0 +1,15 @@
+
+
+
+
+class exe001 {
+    public static void main(String[] args) {
+        float f = 10;
+        int r = 0;
+
+           for (int i = 1; i < 10; i++) {
+                f = f * i;
+           }
+        System.out.printf("%f\n",2f, f);        
+    }
+}
